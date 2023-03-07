db4199b2d90083f348da4f0bb8b5ee2245588dc6
Anton <tepa-86@mail.ru>
2023-03-07 22:11:47 +0300

    Добавление изменение в проект:
    - в файл exchange_rates.py добавлен вывод дополнительных валютных пар:
       EURUSD, GBPUSD, USDCHF, USDCNY, USDTRY, USDJPY, USDHKD, USDSGD, USDAED
    
    - в файле exchange_rates.py добавлен отлов исключений в случае:
      1. отсутствия связи, неверно указанного api_key, ошибки в указании url;
      2. отсутствия котировки по определенной валютной паре
    
    - Добавлены скрины по ошибкам;
    
    - Добавление информации в файл README.md.

--- exchange_rates.py
+++ exchange_rates.py
@@ -1,18 +1,52 @@
 import requests
 
-# Получение API ключа
-api_key = "YOUR_API_KEY"
+api_key = "cc4f090534034bb98c9c5830b036de0c"
+symbols = "USD,RUB,EUR,CNY,TRY,GBP,CHF,JPY,AED,HKD,SGD"
 
-# Формирование URL для получения котировок
-url = f"https://openexchangerates.org/api/latest.json?app_id={api_key}&symbols=USD,RUB,EUR,CNY,TRY,GBP,CHF,JPY,AED,HKD,SGD"
+#Функция получения курса валют
+def get_exchange_rates(api_key: str, symbols: str):
+  
+    # Формирование URL для получения котировок
+    url = f"https://openexchangerates.org/api/latest.json?app_id={api_key}&symbols={symbols}"
 
-# Выполнение запроса к API
-request = requests.get(url)
+    try:
+        # Выполнение запроса к API
+        request = requests.get(url)
 
-# Получение ответа в формате JSON
-received_response = request.json()
+        # Проверка корректности ответа
+        if request.status_code == 200 and request.content:
+          
+            # Получение ответа в формате JSON
+            received_response = request.json()
 
-# Извлечение нужных котировок и вывод на экран
-for currency in ["USD", "EUR", "CNY", "TRY", "GBP", "CHF", "JPY", "AED", "HKD", "SGD"]:
-    exchange_rate = received_response["rates"]["RUB"] / received_response["rates"][currency]
-    print(f"{currency}/RUB: {exchange_rate:.4f}")
+            # Извлечение нужных котировок и вывод на экран
+            for currency in ["USD", "EUR", "CNY", "TRY", "GBP", "CHF", "JPY", "AED", "HKD", "SGD"]:
+                try:
+                    exchange_rate = received_response["rates"]["RUB"] / received_response["rates"][currency]
+                    print(f"{currency}/RUB: {exchange_rate:.4f}")
+                except KeyError:
+                    print(f"{currency}/RUB: котировка отсутствует")
+
+            print()
+
+            for currency in ["EUR", "GBP"]:
+                try:
+                    exchange_rate = received_response["rates"]["USD"] / received_response["rates"][currency]
+                    print(f"{currency}/USD: {exchange_rate:.4f}")
+                except KeyError:
+                    print(f"{currency}/RUB: котировка отсутствует")
+
+            for currency in ["CHF", "CNY", "TRY", "JPY", "HKD", "SGD", "AED"]:
+                try:
+                    exchange_rate = received_response["rates"][currency] / received_response["rates"]["USD"]
+                    print(f"USD/{currency}: {exchange_rate:.4f}")
+                except KeyError:
+                    print(f"{currency}/RUB: котировка отсутствует")
+
+        else:
+            print("\nОшибка при получении котировок")
+
+    except requests.exceptions.ConnectionError:
+        print("Отсутствует связь с сервисом по предоставлению валютных котировок")
+
+get_exchange_rates(api_key, symbols)
